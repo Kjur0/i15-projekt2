ef2707570eadd9e2a0184662645a50f19c4d1b56
Kjur0 <abermkju1@gmail.com>
2022-11-10 22:04:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# **Praca wykonana przez Krzyzstofa Jurkowskiego**
+
+> IDE: **Visual Studio 2022**
+> Kompilator: **Visual C++ 2022**
+> Debugger: **Lokalny Debugger Windows**
+> Kodowanie: **UTF-8**
+> Kompatybilność: **x64**
+
+Użyte bibiloteki:
+
+* iostream
+* cmath
---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	float x, y;
+
+	wcout << "Proszę podać wartość x: ";
+	cin >> x;
+
+	if (x > 0)
+	{
+		y = 2 * sin(x);
+	}
+	else
+	{
+		y = 6 - x;
+	}
+
+	cout << "f(x) = " << y;
+
+	return 0;
+}
